325f97bef0fe55c0bc5551e8e4b549168ee3b588
Lucio Araujo <lucio_a.khfzo@aleeas.com>
2024-07-26 16:18:36 +0000

    deleted api

--- api/Exo.WebApi/atividade-05/Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-namespace Exo.WebApi
-{
-    public class Usuario
-    {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
-    }
-}
