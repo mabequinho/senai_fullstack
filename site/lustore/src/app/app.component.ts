7fa773098cb930f6e5cf623ea6d6c0e4f2f14674
Lucio Araujo <lucio_a.khfzo@aleeas.com>
2024-07-26 17:39:06 +0000

    barra inicial, menu

--- site/lustore/src/app/app.component.ts
+++ site/lustore/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { MenuComponent } from "./menu/menu.component";
+import { InicioComponent } from "./inicio/inicio.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, InicioComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
