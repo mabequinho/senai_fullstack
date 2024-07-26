25ad5cca04ab4d119f7c3b45b004555270afd0b8
Lucio Araujo <lucio_a.khfzo@aleeas.com>
2024-07-26 19:32:41 +0000

    grid produtos import

--- site/lustore/src/app/app.component.ts
+++ site/lustore/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { MenuComponent } from "./menu/menu.component";
 import { InicioComponent } from "./inicio/inicio.component";
+import { GridprodutosComponent } from "./gridprodutos/gridprodutos.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, InicioComponent],
+  imports: [
+    RouterOutlet,
+    MenuComponent,
+    InicioComponent,
+    GridprodutosComponent,
+  ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
