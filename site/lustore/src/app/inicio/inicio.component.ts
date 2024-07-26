f497fc654a997bcd7b23d935933d415d9a7b5716
Lucio Araujo <lucio_a.khfzo@aleeas.com>
2024-07-26 20:29:05 +0000

    carrocel, simplificado

--- site/lustore/src/app/inicio/inicio.component.ts
+++ site/lustore/src/app/inicio/inicio.component.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core";
-import { NgbCarouselModule } from "@ng-bootstrap/ng-bootstrap";
+import {
+  NgbCarouselModule,
+  NgbCarouselConfig,
+} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-inicio",
@@ -7,11 +10,20 @@ import { NgbCarouselModule } from "@ng-bootstrap/ng-bootstrap";
   imports: [NgbCarouselModule],
   templateUrl: "./inicio.component.html",
   styleUrl: "./inicio.component.css",
+  providers: [NgbCarouselConfig], // add NgbCarouselConfig to the component providers
 })
 export class InicioComponent {
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
   images = [
-    "/assets/img/img_02.webp",
-    "/assets/img/img_05.webp",
-    "/assets/img/img_07.webp",
+    "/assets/img/banner01.webp",
+    "/assets/img/banner02.webp",
+    "/assets/img/banner03.webp",
   ];
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 1600;
+    config.showNavigationArrows = false;
+    config.showNavigationIndicators = false;
+  }
 }
