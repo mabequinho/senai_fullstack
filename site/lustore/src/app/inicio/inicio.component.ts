4a93105a92fe8ca996dd4a3f197912e2cfdc193b
Lucio Araujo <lucio_a.khfzo@aleeas.com>
2024-07-26 17:42:02 +0000

    inicio, carrocel exemplo https://ng-bootstrap.github.io/#/components/carousel/examples

--- site/lustore/src/app/inicio/inicio.component.ts
+++ site/lustore/src/app/inicio/inicio.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgbCarouselModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-inicio',
+  selector: "app-inicio",
   standalone: true,
-  imports: [],
-  templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  imports: [NgbCarouselModule],
+  templateUrl: "./inicio.component.html",
+  styleUrl: "./inicio.component.css",
 })
 export class InicioComponent {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
